f20d8381a1e8913ede2aa7e18793c2e7bf34c3af
PedroAdair <pedro.adair.avila@gmail.com>
2024-02-26 13:00:41 -0600

    v 0.2.3 | WhatsApp notification, first aproach

---
+++ Exp3/whats.py
@@ -0,0 +1,10 @@
+import pywhatkit
+
+# Número de teléfono del contacto
+phone_number = "2462562255"
+
+# Mensaje a enviar
+message = "Hola, ¿cómo estás?"
+
+# Enviar el mensaje
+pywhatkit.sendwhatsappmsg(phone_number, message)
